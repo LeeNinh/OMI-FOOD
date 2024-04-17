090e9cc8e6bebd2a032afef1ced859f76cc39147
Ninhld <ninhld@ominext.com>
2024-04-17 15:35:23 +0700

    Ninhld

---
+++ src/main/java/com/example/beomifood/BeOmiFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.beomifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeOmiFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeOmiFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beomifood/BeOmiFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beomifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeOmiFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
